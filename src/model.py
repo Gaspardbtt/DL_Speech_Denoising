12d981cffd0743d151f564be9b8f3ed6f2e034df
Gaspard Botté <gaspardbotte@Air-de-Gaspard.lan>
2025-11-16 15:04:23 +0100

    add files model.py + train.py

---
+++ src/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+#-------------------------------------------------------------------------------------
+
+# CNN arch for Mask approche
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)  
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(8, 1, kernel_size=3, padding=1)  
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.sigmoid(self.conv4(x))
+        return x
+    
+
+#-------------------------------------------------------------------------------------
---
+++ src/train.py
@@ -0,0 +1,6 @@
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.preprocessing import MinMaxScaler
+
+
